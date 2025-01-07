85f71651c93394bbf84dd034cae26e725c2ffb8c
arnoldjanik <arnold.o.janik@gmail.com>
2025-01-07 14:47:15 +0100

    Eligibility verification page tests updated

---
+++ pages/Eligibility.page.ts
@@ -0,0 +1,28 @@
+import { Page } from '@playwright/test';
+
+export class EligibilityPageVerification {
+    constructor(private page: Page) {}
+
+    headerText = this.page.getByRole('heading', { name: 'Just a few more Questions' });
+    logo = this.page.getByRole('img', { name: 'Logo Hippo' });
+    phoneNumber = this.page.getByRole('link', { name: 'Phone 1-877-838-' });
+    agentId = this.page.getByText('Your Agent • Reference ID:');
+    disclaimer = this.page.getByText('Hippo Insurance Services (“');
+    
+    continueButton = this.page.getByRole('button', { name: 'Continue' });
+
+}
+export class EligibilityPageDefaultAnswers {
+    constructor(private page: Page) {}
+
+    hasNonOwnerOccupants = this.page.getByTestId('hasNonOwnerOccupants-No');
+    isBusiness = this.page.getByTestId('isBusiness-No');
+    hasSwimmingPool = this.page.getByTestId('hasSwimmingPool-No');
+    hasTrampoline = this.page.getByTestId('hasTrampoline-No');
+    hasDogs = this.page.getByTestId('hasDogs-No');
+    hasExoticPets = this.page.getByTestId('hasExoticPets-No');
+
+}
+
+
+
---
+++ tests/bbx_eligibility_verfiy.spec.ts
@@ -0,0 +1,59 @@
+import { test, expect } from '@playwright/test';
+import {
+  EligibilityPageDefaultAnswers,
+  EligibilityPageVerification,
+} from '../pages/Eligibility.page';
+
+test.describe(' Default Eligibility page verification ', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(
+      'https://bbx-consent-flow.dev.hippo-private.com/eligibility'
+    );
+  });
+
+  test('Eligibility page verification', async ({ page }) => {
+    //Arrange
+    const consentPage = new EligibilityPageVerification(page);
+    const welcomeText = 'Just a few more Questions';
+    const button = 'Continue';
+    const number = 'Phone 1-877-838-8866';
+    //Act
+
+    //Assert
+    await expect(consentPage.headerText).toHaveText(welcomeText);
+    await expect(consentPage.logo).toBeVisible();
+    await expect(consentPage.phoneNumber).toHaveText(number);
+    await expect(consentPage.agentId).toBeVisible();
+    await expect(consentPage.disclaimer).toHaveText(
+      'Hippo Insurance Services (“Hippo”) is a general agent for affiliated and non-affiliated insurance companies. Hippo is licensed as a property casualty insurance agency in all states in which products are offered.'
+    );
+    await expect(consentPage.continueButton).toHaveText(button);
+  });
+
+  test('Eligibility page default answers verification ', async ({ page }) => {
+    // Arrange
+    const consentPageAnswers = new EligibilityPageDefaultAnswers(page);
+
+    // Act
+
+    // Assert
+
+    await expect.soft(consentPageAnswers.hasNonOwnerOccupants).toBeChecked();
+    await expect.soft(consentPageAnswers.isBusiness).toBeChecked();
+    await expect.soft(consentPageAnswers.hasSwimmingPool).toBeChecked();
+    await expect.soft(consentPageAnswers.hasTrampoline).toBeChecked();
+    await expect.soft(consentPageAnswers.hasDogs).toBeChecked();
+    await expect.soft(consentPageAnswers.hasExoticPets).toBeChecked();
+  });
+
+  test('Eligibility page transition verification', async ({ page }) => {
+    //Arrange
+    const pageTransition = new EligibilityPageVerification(page);
+
+    //Act
+    await pageTransition.continueButton.click();
+
+    //Assert
+    
+  });
+});
