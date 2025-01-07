296b157ce399a576a915d64169107418f522e3b9
arnoldjanik <arnold.o.janik@gmail.com>
2025-01-07 20:13:45 +0100

    1st eligibility form test missing { fix

--- tests/eligibility_form.spec.ts
+++ tests/eligibility_form.spec.ts
@@ -9,27 +9,25 @@ test.describe('Eligibility page form options verification', () => {
     await page.goto(
       'https://bbx-consent-flow.dev.hippo-private.com/eligibility'
     );
-    test('Non-owner occupied Yes, 1st opt No, 2nd opt No', async ({ page }) => {
-      //Arrange
-      const formOption = new EligibilityPageAnswers(page);
-      const pageTransition = new EligibilityPageVerification(page);
-      //Act
-      await formOption.hasNonOwnerOccupantsYes.click();
-      await formOption.secondHomeNo.click();
-      await formOption.vacantNo.click();
-      await pageTransition.continueButton.click();
+  });
+  test('Non-owner occupied Yes, 1st opt No, 2nd opt No', async ({ page }) => {
+    //Arrange
+    const formOption = new EligibilityPageAnswers(page);
+    const pageTransition = new EligibilityPageVerification(page);
+    //Act
+    await formOption.hasNonOwnerOccupantsYes.click();
+    await formOption.secondHomeNo.click();
+    await formOption.vacantNo.click();
+    await pageTransition.continueButton.click();
 
-      //Assert
-      await expect(pageTransition.checkoutHeader).toHaveText(
-        'Confirm your insurance application'
-      );
-    });
-    test('non-owner occupied Yes, 1st opt Yes, 2nd opt No', async ({
-      page,
-    }) => {
-      //Arrange
-      //Act
-      //Assert
-    });
+    //Assert
+    await expect(pageTransition.checkoutHeader).toHaveText(
+      'Confirm your insurance application'
+    );
+  });
+  test('non-owner occupied Yes, 1st opt Yes, 2nd opt No', async ({ page }) => {
+    //Arrange
+    //Act
+    //Assert
   });
 });
