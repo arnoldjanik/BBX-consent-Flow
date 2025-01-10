22936c281e98c7ad27e8ac1bdf3ff963a88f77b5
arnoldjanik <arnold.o.janik@gmail.com>
2025-01-10 11:59:18 +0100

    assertion code refactor

--- tests/eligibility_form.spec.ts
+++ tests/eligibility_form.spec.ts
@@ -22,72 +22,56 @@ test.describe('Eligibility page form options verification', () => {
     await nonOwnerOccupants({ page: page, secondHome: false, vacant: false });
 
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('Non-owner occupied Yes, 1st opt Yes, 2nd opt No', async ({ page }) => {
     //Act
     await nonOwnerOccupants({ page: page, secondHome: true, vacant: false });
 
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('Non-owner occupied Yes, 1st opt Yes, 2nd opt Yes', async ({ page }) => {
     //Act
     await nonOwnerOccupants({ page: page, secondHome: true, vacant: true });
 
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('Non-owner occupied Yes, 1st opt No, 2nd opt Yes', async ({ page }) => {
     //Act
     await nonOwnerOccupants({ page: page, secondHome: false, vacant: true });
 
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('Has Swimming Pool Yes, 1st opt No, 2nd opt No', async ({ page }) => {
     //Act
     await swimmingPool({ page: page, fence: false, divingBoard: false });
 
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('Has Swimming Pool yes, 1st opt Yes, 2nd opt No', async ({ page }) => {
     //Act
     await swimmingPool({ page: page, fence: true, divingBoard: false });
 
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('Has Swimming Pool yes, 1st opt Yes, 2nd opt Yes', async ({ page }) => {
     //Act
     await swimmingPool({ page: page, fence: true, divingBoard: true });
 
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('Has Swimming Pool yes, 1st opt No, 2nd opt Yes', async ({ page }) => {
     //Act
     await swimmingPool({ page: page, fence: false, divingBoard: true });
 
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
 
   test('Is business Yes', async ({ page }) => {
@@ -102,61 +86,47 @@ test.describe('Eligibility page form options verification', () => {
     await hasTrampolineYes({ page });
 
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('has Dogs Yes Pure breed bite yes ', async ({ page }) => {
     //Act
     await dogBreed({ page: page, pureBreed: true, bite: true });
 
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('has Dogs Yes Pure breed bite no ', async ({ page }) => {
     //Act
     await dogBreed({ page: page, pureBreed: true, bite: false });
 
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('has Dog Yes Mixed breed bite yes ', async ({ page }) => {
     //Act
     await dogBreed({ page: page, pureBreed: false, bite: true });
 
     // Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('has Dog yes mixed breed no bite', async ({ page }) => {
     //Act
     await dogBreed({ page: page, pureBreed: false, bite: false });
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('has 2nd Dog', async ({ page }) => {
     //Act
     await anotherDog({ page: page, otherDog: true });
 
     // Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
   test('2nd dog row removal', async ({ page }) => {
     //Act
     await anotherDog({ page: page, otherDog: false });
 
     //Assert
-    await expect(page.locator(eligibilityLocators.checkoutHeader)).toHaveText(
-      'Confirm your insurance application'
-    );
+    await assertCheckoutHeader(page, 'Confirm your insurance application');
   });
 });
